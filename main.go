79be70dadda7f8fb136682b81ed86cec6d421166
FarhadF <farhad.farahi@gmail.com>
2018-04-29 19:54:33 +0430

    sample logging

--- main.go
+++ main.go
@@ -3,16 +3,22 @@ package main
 import (
 	"net/http"
 	httptransport "github.com/go-kit/kit/transport/http"
-	"log"
+	"github.com/go-kit/kit/log"
 	"context"
 	"encoding/json"
+	"github.com/go-kit/kit/endpoint"
+	"os"
 )
 
 func main(){
 	svc := stringService{}
+	logger := log.NewLogfmtLogger(os.Stderr)
+	var uppercase endpoint.Endpoint
+	uppercase = makeUppercaseEndpoint(svc)
+	uppercase = loggingMiddleware(log.With(logger, "method", "uppercase"))(uppercase)
 	//each service method needs a handler
 	uppercaseHandler := httptransport.NewServer(
-		makeUppercaseEndpoint(svc),
+		uppercase,
 		decodeUppercaseRequest,
 		encodeResponse,
 	)
@@ -25,7 +31,7 @@ func main(){
 	http.Handle("/uppercase", uppercaseHandler)
 	//POST /count -d '{"str":"string"}'
 	http.Handle("/count", countHandler)
-	log.Fatal(http.ListenAndServe(":8080",nil))
+	logger.Log("err",http.ListenAndServe(":8081",nil))
 }
 //request and response decoder/encoders
 func decodeUppercaseRequest(_ context.Context, r *http.Request) (interface{}, error) {
